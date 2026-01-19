0a6027545f203008eb4188fcf222f8ba5d053ee2
vennby <f20220233@dubai.bits-pilani.ac.in>
2026-01-19 22:59:29 +0530

    mail-interface

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
@@ -7,25 +7,58 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'microsofttechclub@dubai.bits-pilani.ac.in'
-app.config['MAIL_PASSWORD'] = 'rbnj oaoo uekf mtvu'
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD")
+)
 
 mail = Mail(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Example predefined groups
+RECIPIENT_GROUPS = {
+    "All Members": [
+        "f20220233@dubai.bits-pilani.ac.in",
+        "f20220234@dubai.bits-pilani.ac.in"
+    ],
+    "Core Team": [
+        "core1@dubai.bits-pilani.ac.in",
+        "core2@dubai.bits-pilani.ac.in"
+    ]
+}
 
-@app.route('/send')
-def send_email(): 
+@app.route("/")
+def index():
+    return render_template("index.html", groups=RECIPIENT_GROUPS)
+
+@app.route("/send", methods=["POST"])
+def send_email():
+    to = request.form.get("to", "").split(",")
+    cc = request.form.get("cc", "").split(",")
+    bcc = request.form.get("bcc", "").split(",")
+    subject = request.form.get("subject")
+    body = request.form.get("body")
+
     msg = Message(
-        subject='Hello!',
-        sender='microsofttechclub@dubai.bits-pilani.ac.in',
-        recipients=['f20220233@dubai.bits-pilani.ac.in'],
-        body='This email was sent automatically through Flask!'
+        subject=subject,
+        sender=app.config["MAIL_USERNAME"],
+        recipients=[e for e in to if e],
+        cc=[e for e in cc if e],
+        bcc=[e for e in bcc if e],
+        html=body
     )
+
+    # Attachments
+    files = request.files.getlist("attachments")
+    for file in files:
+        if file.filename:
+            msg.attach(
+                file.filename,
+                file.content_type,
+                file.read()
+            )
+
     mail.send(msg)
-    return render_template('sent.html')
+    return render_template("sent.html")
---
+++ old_app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'microsofttechclub@dubai.bits-pilani.ac.in'
+app.config['MAIL_PASSWORD'] = 'rbnj oaoo uekf mtvu'
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send')
+def send_email(): 
+    msg = Message(
+        subject='Hello!',
+        sender='microsofttechclub@dubai.bits-pilani.ac.in',
+        recipients=['f20220233@dubai.bits-pilani.ac.in'],
+        body='This email was sent automatically through Flask!'
+    )
+    mail.send(msg)
+    return render_template('sent.html')
+
